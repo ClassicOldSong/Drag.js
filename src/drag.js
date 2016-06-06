b39a407899e4946377179e7abc8e96916b6e0a91
Yukino Song <syqlds@126.com>
2016-06-06 23:13:31 +0800

    Better logic

--- src/drag.js
+++ src/drag.js
@@ -1,6 +1,7 @@
-/* global console, $ */
+/* global $q, $create, $ */
 "use strict";
 {
+	window.maxDeg = 0;
 	let mouseX = 0, mouseY = 0;
 	document.addEventListener('mousemove', (e) => {
 		mouseX = e.clientX;
@@ -9,37 +10,46 @@
 
 	const createCase = (node) => {
 		let dragCase = $create('dragcase');
-		let style = window.getComputedStyle(node);
-		dragCase.dragger = {};
-		dragCase.dragger.posX = node.getBoundingClientRect().left;
-		dragCase.dragger.posY = node.getBoundingClientRect().top;
-		dragCase.dragger.deg = 0;
+		let dragHolder = $create('dragHolder');
+		let clientStyle = node.getBoundingClientRect();
+		dragCase.dragger = {
+			posX: clientStyle.left,
+			posY: clientStyle.top,
+			startX: clientStyle.left,
+			startY: clientStyle.top,
+			deg: 0
+		};
 		dragCase.style.position = 'fixed';
 		dragCase.style.display = 'block';
 		dragCase.style.left = dragCase.dragger.posX + 'px';
 		dragCase.style.top = dragCase.dragger.posY + 'px';
+		dragCase.append(dragHolder);
+		node.dragCase = dragCase;
+		node.dragHolder = dragHolder;
 		return dragCase;
 	};
 
 	const drag = function() {
 		let diffX = 0, diffY = 0;
 
-		if (this.dragging === false) {
-			diffX = this.dragger.targetX - this.clientWidth/2 - this.dragger.posX;
+		if (this.dragging) {
+			diffX = mouseX - this.clientWidth/2 - this.dragger.posX;
+			diffY = mouseY - this.dragger.posY;
+		} else {
+			diffX = this.dragger.targetX - this.dragger.posX;
 			diffY = this.dragger.targetY - this.dragger.posY;
 			if ((Math.pow(diffX, 2) + Math.pow(diffY, 2)) < 0.05) {
-				this.style.left = this.dragger.targetX - this.clientWidth/2 + 'px';
+				this.style.left = this.dragger.targetX + 'px';
 				this.style.top = this.dragger.targetY + 'px';
 				this.style.transform = `rotate(0deg)`;
 				window.cancelAnimationFrame(this.dragID);
+				this.dragger.dragID = null;
+				if (this.dragger.callback) this.dragger.callback();
 				return;
 			}
-		} else {
-			diffX = mouseX - this.clientWidth/2 - this.dragger.posX;
-			diffY = mouseY - this.dragger.posY;
 		}
 
-		let diffDeg = Math.tan(diffX/Math.abs(this.dragger.posY - mouseY - 500))/Math.PI*180;
+		let diffDeg = Math.tan(diffX/(Math.abs(this.dragger.posY - mouseY) + this.clientHeight*3 + 200 + Math.abs(diffX)))/Math.PI*180;
 		if (diffDeg > 60) {
 			diffDeg = 60;
 		} else if (diffDeg < -60) {
@@ -53,7 +63,7 @@
 		this.style.top = this.dragger.posY + 'px';
 		this.style.transform = `rotate(${this.dragger.deg}deg)`;
 
-		this.dragID = window.requestAnimationFrame(drag.bind(this));
+		this.dragger.dragID = window.requestAnimationFrame(drag.bind(this));
 	};
 
 	$.fn('Dragjs', {
@@ -66,23 +76,37 @@
 					dragCase = this.parentNode;
 				} else {
 					dragCase = createCase(this);
+					this.swap(this.dragHolder);
 				}
 				$q('body').appendChild(dragCase);
-				dragCase.append(this);
 				dragCase.dragging = true;
-				drag.call(dragCase);
+				if (!dragCase.dragger.dragID) {
+					drag.call(dragCase);
+				}
 				return this;
 			},
-			dragRelease() {
+			dragRelease(cb) {
 				if (this.parentNode.tagName.toUpperCase() === 'DRAGCASE' && this.parentNode.dragging === true) {
-					this.parentNode.dragger.targetX = mouseX;
-					this.parentNode.dragger.targetY = mouseY;
-					this.parentNode.dragging = false;
+					this.dragCase.dragger.callback = cb;
+					this.dragCase.dragger.targetX = mouseX;
+					this.dragCase.dragger.targetY = mouseY;
+					this.dragCase.dragging = false;
 				}
 			},
-			stopDrag() {
-				if (this.parentNode.tagName.toUpperCase() === 'DRAGCASE') {
-					window.cancelAnimationFrame(this.parentNode.dragID);
+			stopDrag(cb) {
+				if (this.dragCase) {
+					if (this.dragCase.dragger.dragID) {
+						window.cancelAnimationFrame(this.dragCase.dragger.dragID);
+					}
+					if (cb) {
+						cb();
+					} else {
+						this.dragHolder.swap(this);
+					}
+					this.dragCase.remove();
+					this.dragHolder.remove();
+					this.dragCase = null;
+					this.dragHolder = null;
 				}
 			}
 		}
